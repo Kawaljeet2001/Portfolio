99448a58c13c58932a4f630f1a07b87d112313b6
Kawaljeet Singh Batra <batrakawaljeetsingh@gmail.com>
2021-03-09 22:26:19 +0530

    added the profiles section and email div

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
     return (
         <div className = "About">
-            <h3>About-Me</h3>
+            {/* <h3>About-Me</h3> */}
         </div>
     )
 }
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -5,7 +5,16 @@ const Landing = () => {
     
     return (
         <div className = "Landing"> 
-            ergeg
+            <div className = "profiles-div">
+                <i className="fa fa-github fa-lg" aria-hidden="true"></i>
+                <i className="fa fa-linkedin fa-lg" aria-hidden="true"></i>
+                <i className="fa fa-instagram fa-lg" aria-hidden="true"></i>
+                <div className = "vl"></div>
+            </div>
+            <div className = "email">
+                <p>batrakawaljeetsingh@gmail.com</p>
+                <div className = "vl"></div>
+            </div>
         </div>
     )
 }
